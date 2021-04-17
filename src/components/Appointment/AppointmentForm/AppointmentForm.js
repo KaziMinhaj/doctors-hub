dd51f90846f0771f093df9a63df4470411cf69f6
Kazi Minhaj <kaziminhajulhaider@gmail.com>
2021-04-18 00:42:07 +0600

    52_7

--- src/components/Appointment/AppointmentForm/AppointmentForm.js
+++ src/components/Appointment/AppointmentForm/AppointmentForm.js
@@ -23,8 +23,22 @@ const AppointmentForm = ({ date, modalIsOpen, closeModal, appointmentOn }) => {
   } = useForm();
 
   const onSubmit = (data) => {
-    console.log(data);
-    closeModal();
+    data.service = appointmentOn;
+    data.date = date;
+    data.created = new Date();
+
+    fetch("http://localhost:5000/addAppointment", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((success) => {
+        if (success) {
+          closeModal();
+          alert("appointment created successfully");
+        }
+      });
   };
 
   return (
